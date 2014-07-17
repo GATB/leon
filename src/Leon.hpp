9d5d3b23a3aee7fba878b4879154a4a14553db72
Guillaume Rizk <guillaume.rizk@gmail.com>
2014-07-17 14:10:20 +0200

    other deadlock

--- src/Leon.hpp
+++ src/Leon.hpp
@@ -68,7 +68,7 @@ class Leon : public misc::impl::Tool
 		
 		size_t          _kmerSize;
 		string     _dskOutputFilename;
-		static const int READ_PER_BLOCK = 30000;
+		static const int READ_PER_BLOCK = 50000;
 		int _nb_cores;
 		
 		bool _compress, _decompress;
