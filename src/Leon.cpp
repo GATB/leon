ea32c096478dade9e56fc7a48ee9c64751e2b504
Claire Lemaitre <claire.lemaitre@inria.fr>
2015-03-11 16:44:15 +0100

    min_auto_cutoff set to 4

--- src/Leon.cpp
+++ src/Leon.cpp
@@ -294,6 +294,7 @@ void Leon::createBloom (){
 		}
 		//////
 		
+
 	}
 	else
 	{
@@ -647,7 +648,7 @@ void Leon::executeCompression(){
         );
 
         sortingCount.getInput()->add (0, STR_VERBOSE, getInput()->getStr(STR_VERBOSE));
-
+        sortingCount.setMinAutoThreshold(4); // best compression ratio if min-abundance never below 4 (ie. each kmer of the graph is used by at least 4 reads)
         sortingCount.execute();
     }
 
